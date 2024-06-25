ac695a733bfc1b22407868d64190834af69c780a
xskuy <benj123a@gmail.com>
2024-06-24 22:28:53 -0400

    chore: Update index.html, urls.py, and views.py

--- pagina/urls.py
+++ pagina/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('productos/', views.productos, name='productos'),
+    path('nosotros/', views.nosotros, name='nosotros'),
 ]
--- pagina/views.py
+++ pagina/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def productos(request):
+    return render(request, 'productos.html')
+
+def nosotros(request):
+    return render(request, 'nosotros.html')
